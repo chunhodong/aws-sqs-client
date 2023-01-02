da96c2960790fc3743d056c05be8c43b0e637655
chunhodong <yhms4432@naver.com>
2023-01-02 20:04:00 +0900

    feat: AwsSQSClientTemplate생성자에 객체생성코드추가

--- src/main/java/com/github/chunhodong/awssqsclient/client/AwsSQSClientTemplate.java
+++ src/main/java/com/github/chunhodong/awssqsclient/client/AwsSQSClientTemplate.java
@@ -1,9 +1,34 @@
 package com.github.chunhodong.awssqsclient.client;
 
+import com.amazonaws.services.sqs.buffered.AmazonSQSBufferedAsyncClient;
+import com.github.chunhodong.awssqsclient.pool.AwsSQSClientPool;
+
+import java.util.Objects;
+
 public class AwsSQSClientTemplate {
 
-    public AwsSQSClientTemplate(AwsSQSClientTemplateBuilder builder) {
+    private static final int DEFAULT_MAX_POOL_SIZE = 100;
+    private static final int DEFAULT_MIN_POOL_SIZE = 1;
+    private final String channel;
+    private final AwsSQSClientPool clientPool;
 
+    public AwsSQSClientTemplate(AwsSQSClientTemplateBuilder builder) {
+        validationAttribute(builder);
+        this.channel = builder.channel;
+        this.clientPool = createPool(builder);
+    }
+
+    private AwsSQSClientPool createPool(AwsSQSClientTemplateBuilder builder) {
+        return null;
+    }
+
+    private void validationAttribute(AwsSQSClientTemplateBuilder builder) {
+        Objects.requireNonNull(builder);
+        Objects.requireNonNull(builder.asyncClient);
+        Objects.requireNonNull(builder.channel);
+        if (builder.isFixedPoolsize) {
+            Objects.requireNonNull(builder.poolSize);
+        }
     }
 
     public static AwsSQSClientTemplateBuilder builder() {
@@ -12,16 +37,28 @@ public class AwsSQSClientTemplate {
 
     public static class AwsSQSClientTemplateBuilder {
 
-        private Integer maxPoolSize;
+        private Integer poolSize;
         private String channel;
+        private AmazonSQSBufferedAsyncClient asyncClient;
+        private boolean isFixedPoolsize;
 
-        public AwsSQSClientTemplateBuilder maxPoolSize(int maxPoolSize) {
-            this.maxPoolSize = maxPoolSize;
+        public AwsSQSClientTemplateBuilder poolSize(int poolSize) {
+            this.poolSize = poolSize;
             return this;
         }
 
         public AwsSQSClientTemplateBuilder channel(String channel) {
             this.channel = channel;
+            return this;
+        }
+
+        public AwsSQSClientTemplateBuilder asyncClient(AmazonSQSBufferedAsyncClient asyncClient) {
+            this.asyncClient = asyncClient;
+            return this;
+        }
+
+        public AwsSQSClientTemplateBuilder isFixedPoolsize(boolean isFixedPoolsize) {
+            this.isFixedPoolsize = isFixedPoolsize;
             return this;
         }
 

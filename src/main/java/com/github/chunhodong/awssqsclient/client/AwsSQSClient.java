d7becfae08833a1e6a9b24e8d5a7c4d971fc862b
chunhodong <yhms4432@naver.com>
2023-01-04 21:40:11 +0900

    feat: SQSClient클래스추가
    - aws sqs로 메시지전송 담당하는 인스턴스추가

--- src/main/java/com/github/chunhodong/awssqsclient/client/AwsSQSClient.java
+++ src/main/java/com/github/chunhodong/awssqsclient/client/AwsSQSClient.java
@@ -1,16 +1,21 @@
 package com.github.chunhodong.awssqsclient.client;
 
+import com.github.chunhodong.awssqsclient.message.PushMessage;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 
-import java.io.PushbackInputStream;
 import java.util.Objects;
 
-public class AwsSQSClient implements SQSClient {
+public class AwsSQSClient implements SQSClient<PushMessage> {
 
     private QueueMessagingTemplate queueMessagingTemplate;
 
     public AwsSQSClient(QueueMessagingTemplate queueMessagingTemplate) {
         Objects.nonNull(queueMessagingTemplate);
         this.queueMessagingTemplate = queueMessagingTemplate;
+    }
+
+    @Override
+    public void send(String channel, PushMessage pushMessage) {
+
     }
 }

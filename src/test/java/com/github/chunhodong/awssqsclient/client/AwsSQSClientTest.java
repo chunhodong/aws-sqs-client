241e5c092b297d52514ad3e15a0932023da5b34c
chunhodong <yhms4432@naver.com>
2023-01-11 22:01:55 +0900

    test: AwsSQSClientTest추가
    - awsclient전송테스트

--- src/test/java/com/github/chunhodong/awssqsclient/client/AwsSQSClientTest.java
+++ src/test/java/com/github/chunhodong/awssqsclient/client/AwsSQSClientTest.java
@@ -1,4 +1,29 @@
 package com.github.chunhodong.awssqsclient.client;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+
+@ExtendWith(MockitoExtension.class)
 public class AwsSQSClientTest {
+    @InjectMocks
+    private AwsSQSClient awsSQSClient;
+    @Mock
+    private QueueMessagingTemplate queueMessagingTemplate;
+
+    @Test
+    @DisplayName("채널과 메시지가 존재하면 메시지를 전송")
+    void createClientTemplate() {
+        doNothing().when(queueMessagingTemplate).convertAndSend(anyString(), (Object) any());
+
+        awsSQSClient.send("test", "test");
+    }
 }

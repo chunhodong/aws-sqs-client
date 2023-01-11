e60ae9b7f0c78402ee5b6de17d03034444ac929a
chunhodong <yhms4432@naver.com>
2023-01-11 22:12:18 +0900

    feat: AwsSQSClient 인스턴스생성 정적펙토리메소드추가

--- src/main/java/com/github/chunhodong/awssqsclient/client/AwsSQSClient.java
+++ src/main/java/com/github/chunhodong/awssqsclient/client/AwsSQSClient.java
@@ -1,20 +1,30 @@
 package com.github.chunhodong.awssqsclient.client;
 
+import com.amazonaws.services.sqs.buffered.AmazonSQSBufferedAsyncClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 
+@Component
+@RequiredArgsConstructor
 public class AwsSQSClient implements SQSClient {
 
-    private QueueMessagingTemplate queueMessagingTemplate;
-
-    public AwsSQSClient(QueueMessagingTemplate queueMessagingTemplate) {
-        Objects.nonNull(queueMessagingTemplate);
-        this.queueMessagingTemplate = queueMessagingTemplate;
-    }
+    private final QueueMessagingTemplate queueMessagingTemplate;
 
     @Override
     public void send(String channel, Object pushMessage) {
+        validateMessage(channel, pushMessage);
         queueMessagingTemplate.convertAndSend(channel, pushMessage);
+    }
+
+    private void validateMessage(String channel, Object pushMessage) {
+        Objects.nonNull(channel);
+        Objects.nonNull(pushMessage);
+    }
+
+    public static AwsSQSClient createClient(AmazonSQSBufferedAsyncClient asyncClient) {
+        return new AwsSQSClient(new QueueMessagingTemplate(asyncClient));
     }
 }

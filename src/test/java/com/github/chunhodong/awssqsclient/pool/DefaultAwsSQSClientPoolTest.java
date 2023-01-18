38df3f12073d927dc5698c0c4c6a6a5eca1ff8c1
chunhodong <yhms4432@naver.com>
2023-01-18 23:21:19 +0900

    test: DefaultAwsSQSClientPoolTest추가

--- src/test/java/com/github/chunhodong/awssqsclient/pool/DefaultAwsSQSClientPoolTest.java
+++ src/test/java/com/github/chunhodong/awssqsclient/pool/DefaultAwsSQSClientPoolTest.java
@@ -1,16 +1,47 @@
 package com.github.chunhodong.awssqsclient.pool;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import com.amazonaws.services.sqs.buffered.AmazonSQSBufferedAsyncClient;
 import com.github.chunhodong.awssqsclient.client.SQSClient;
-import com.github.chunhodong.awssqsclient.utils.Timeout;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
-public class DefaultAwsSQSClientPoolTest{
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+public class DefaultAwsSQSClientPoolTest {
+
+    @Test
+    @DisplayName("Pool 객체를 생성한다")
+    void createClientTemplate() {
+        AmazonSQSAsyncClientBuilder builder = AmazonSQSAsyncClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("accessKey", "secretKey")));
+        AmazonSQSBufferedAsyncClient asyncClient = new AmazonSQSBufferedAsyncClient(builder.build());
+        List<SQSClient> sqsClients = new ArrayList<>();
+        new DefaultAwsSQSClientPool(100, sqsClients, asyncClient);
+    }
+
+    @Test
+    @DisplayName("Pool에 추가될 entry가 null이면 생성실패")
+    void throwsExceptionWhenEntryIsNull() {
+        AmazonSQSAsyncClientBuilder builder = AmazonSQSAsyncClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("accessKey", "secretKey")));
+        AmazonSQSBufferedAsyncClient asyncClient = new AmazonSQSBufferedAsyncClient(builder.build());
+        assertThatThrownBy(() -> new DefaultAwsSQSClientPool(100, null, asyncClient))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    @DisplayName("Pool에 추가될 async객체가 null이면 생성실패")
+    void throwsExceptionWhenAsyncClientIsNull() {
+        List<SQSClient> sqsClients = new ArrayList<>();
+
+        assertThatThrownBy(() -> new DefaultAwsSQSClientPool(100, sqsClients, null))
+                .isInstanceOf(NullPointerException.class);
+    }
 
 }

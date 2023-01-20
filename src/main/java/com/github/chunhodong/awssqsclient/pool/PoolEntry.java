9c532e9fd9a3b6bc1f82e4b4383408ee90ab38d3
chunhodong <yhms4432@naver.com>
2023-01-20 20:44:55 +0900

    feat: PoolEntry에 락획득메소드 추가

--- src/main/java/com/github/chunhodong/awssqsclient/pool/PoolEntry.java
+++ src/main/java/com/github/chunhodong/awssqsclient/pool/PoolEntry.java
@@ -3,15 +3,19 @@ package com.github.chunhodong.awssqsclient.pool;
 import com.github.chunhodong.awssqsclient.client.SQSClient;
 
 import java.util.Objects;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class PoolEntry {
 
     private final SQSClient sqsClient;
     private PoolEntryState state;
+    private final ReentrantLock accessLock;
 
     public PoolEntry(SQSClient sqsClient) {
         Objects.nonNull(sqsClient);
         this.sqsClient = sqsClient;
+        this.accessLock = new ReentrantLock();
+        this.state = PoolEntryState.IN_NOT_USE;
     }
 
     public SQSClient getSqsClient() {
@@ -28,5 +32,15 @@ public class PoolEntry {
 
     public void unuse() {
         state = PoolEntryState.IN_NOT_USE;
+    }
+
+    public void accessLock() {
+        accessLock.lock();
+    }
+
+    public void releaseLock() {
+        if (accessLock.isLocked()) {
+            accessLock.unlock();
+        }
     }
 }

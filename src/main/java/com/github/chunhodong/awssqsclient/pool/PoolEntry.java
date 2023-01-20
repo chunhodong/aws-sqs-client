ec6d9f4ac86fefbdf52bdf0af4313f0076884395
chunhodong <yhms4432@naver.com>
2023-01-20 19:33:17 +0900

    feat: PoolEntry에 사용여부확인 메소드추가

--- src/main/java/com/github/chunhodong/awssqsclient/pool/PoolEntry.java
+++ src/main/java/com/github/chunhodong/awssqsclient/pool/PoolEntry.java
@@ -7,6 +7,7 @@ import java.util.Objects;
 public class PoolEntry {
 
     private final SQSClient sqsClient;
+    private PoolEntryState state;
 
     public PoolEntry(SQSClient sqsClient) {
         Objects.nonNull(sqsClient);
@@ -15,5 +16,17 @@ public class PoolEntry {
 
     public SQSClient getSqsClient() {
         return sqsClient;
+    }
+
+    public boolean isUse() {
+        return state == PoolEntryState.IN_USE;
+    }
+
+    public void use() {
+        state = PoolEntryState.IN_USE;
+    }
+
+    public void unuse() {
+        state = PoolEntryState.IN_NOT_USE;
     }
 }

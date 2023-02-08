87ea38750baec78569e2f8e7e488d7a08b4dc64b
chunhodong <yhms4432@naver.com>
2023-02-08 21:47:16 +0900

    refactor: FlexibleAwsSQSClientPool에서 ReentrantLock제거
    - lock대신 synchronized블록으로 엔트리추가

--- src/main/java/com/github/chunhodong/awssqsclient/pool/FlexibleAwsSQSClientPool.java
+++ src/main/java/com/github/chunhodong/awssqsclient/pool/FlexibleAwsSQSClientPool.java
@@ -5,23 +5,19 @@ import com.github.chunhodong.awssqsclient.client.SQSClient;
 import com.github.chunhodong.awssqsclient.utils.Timeout;
 
 import java.util.List;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class FlexibleAwsSQSClientPool extends DefaultAwsSQSClientPool {
 
     private final int maxPoolsize;
-    private ReentrantLock entryLock;
 
     public FlexibleAwsSQSClientPool(int maxPoolSize, List<SQSClient> clients, AmazonSQSBufferedAsyncClient asyncClient) {
         super(clients, asyncClient);
         this.maxPoolsize = maxPoolSize;
-        this.entryLock = new ReentrantLock(true);
     }
 
     public FlexibleAwsSQSClientPool(int maxPoolSize, Timeout connectionTimeout, Timeout idleTimeout, List<SQSClient> clients, AmazonSQSBufferedAsyncClient asyncClient) {
         super(clients, asyncClient, connectionTimeout, idleTimeout);
         this.maxPoolsize = maxPoolSize;
-        this.entryLock = new ReentrantLock(true);
     }
 
     @Override
@@ -41,6 +37,7 @@ public class FlexibleAwsSQSClientPool extends DefaultAwsSQSClientPool {
 
         @Override
         public void run() {
+
         }
     }
 }

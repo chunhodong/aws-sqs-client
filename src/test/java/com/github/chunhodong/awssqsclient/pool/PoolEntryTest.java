c0470e306fe82185df0c49370fc07c5ef50337fd
chunhodong <yhms4432@naver.com>
2023-02-14 23:58:06 +0900

    test: PoolEntryTest에서 idle여부테스트

--- src/test/java/com/github/chunhodong/awssqsclient/pool/PoolEntryTest.java
+++ src/test/java/com/github/chunhodong/awssqsclient/pool/PoolEntryTest.java
@@ -1,7 +1,10 @@
 package com.github.chunhodong.awssqsclient.pool;
 
+import com.github.chunhodong.awssqsclient.utils.Timeout;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,6 +29,19 @@ public class PoolEntryTest {
         poolEntry.open();
 
         assertThat(poolEntry.isClose()).isFalse();
+    }
+
+    @Test
+    @DisplayName("풀엔트리가 오픈상태이면서 생성시간이 입력값보다 많으면 true")
+    void returnTrueWhenPoolIsIdle() throws InterruptedException {
+        PoolEntry poolEntry = new PoolEntry((channel, message) -> {
+        });
+
+        poolEntry.open();
+        Thread.sleep(1000);
+
+        assertThat(poolEntry.isIdle(new Timeout(TimeUnit.NANOSECONDS,100l))).isTrue();
+
     }
 
 }

7501c80a79ad6ce584677f7aca3cf91eb14e5837
chunhodong <yhms4432@naver.com>
2023-02-20 23:01:16 +0900

    test: FlexibleAwsSQSClientPoolTest멀티스레드 테스트추가

--- src/test/java/com/github/chunhodong/awssqsclient/pool/FlexibleAwsSQSClientPoolTest.java
+++ src/test/java/com/github/chunhodong/awssqsclient/pool/FlexibleAwsSQSClientPoolTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -30,9 +33,7 @@ public class FlexibleAwsSQSClientPoolTest {
     @DisplayName("Pool에있는 entry를 추가하면 poolSize에 반영")
     void returnPoolSizeWhenAddEntry() {
         AmazonSQSBufferedAsyncClient asyncClient = mock(AmazonSQSBufferedAsyncClient.class);
-        List<SQSClient> sqsClients = Arrays.asList((channel, message) -> {
-        }, (channel, message) -> {
-        });
+        List<SQSClient> sqsClients = Arrays.asList((channel, message) -> { }, (channel, message) -> { });
 
         FlexibleAwsSQSClientPool flexibleAwsSQSClientPool = new FlexibleAwsSQSClientPool(10, sqsClients, asyncClient);
         flexibleAwsSQSClientPool.createEntry();
@@ -41,7 +42,23 @@ public class FlexibleAwsSQSClientPoolTest {
 
     @Test
     @DisplayName("멀티스레드상황에서 entry를 추가하면 maxPoolSize개수까지만 생성")
-    void returnPoolSizeAtMaxPoolSize() {
+    void returnPoolSizeAtMaxPoolSize() throws InterruptedException {
+        AmazonSQSBufferedAsyncClient asyncClient = mock(AmazonSQSBufferedAsyncClient.class);
+        List<SQSClient> sqsClients = Arrays.asList((channel, message) -> { });
+        FlexibleAwsSQSClientPool flexibleAwsSQSClientPool = new FlexibleAwsSQSClientPool(100, sqsClients, asyncClient);
+        int totalNumberOfTasks = 150;
+        ExecutorService executor = Executors.newFixedThreadPool(200);
+
+        CountDownLatch latch = new CountDownLatch(totalNumberOfTasks);
+        for(int i = 0; i < totalNumberOfTasks; i++){
+            executor.submit(() -> {
+                flexibleAwsSQSClientPool.createEntry();
+                latch.countDown();
+            });
+        }
+        latch.await();
+
+        assertThat(flexibleAwsSQSClientPool.getPoolSize()).isEqualTo(100);
     }
 
 }

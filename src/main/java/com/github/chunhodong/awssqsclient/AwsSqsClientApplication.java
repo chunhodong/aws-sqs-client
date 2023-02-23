7154b9ea559a2b27e03f33897e7a99cff1c24be4
chunhodong <yhms4432@naver.com>
2023-02-23 17:01:49 +0900

    refactor: 메인클래스추가

---
+++ src/main/java/com/github/chunhodong/awssqsclient/AwsSqsClientApplication.java
@@ -0,0 +1,14 @@
+package com.github.chunhodong.awssqsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSqsClientApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AwsSqsClientApplication.class, args);
+    }
+
+}

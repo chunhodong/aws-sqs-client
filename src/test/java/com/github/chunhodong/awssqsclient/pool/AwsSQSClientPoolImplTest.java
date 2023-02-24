7fd593cc52d76e2e5b5bf954bb7fb6f8c389739a
chunhodong <yhms4432@naver.com>
2023-02-24 17:23:05 +0900

    test: AwsSQSClientPoolImplTest 풀사이즈 테스트삭제

--- src/test/java/com/github/chunhodong/awssqsclient/pool/AwsSQSClientPoolImplTest.java
+++ src/test/java/com/github/chunhodong/awssqsclient/pool/AwsSQSClientPoolImplTest.java
@@ -1,22 +1,5 @@
 package com.github.chunhodong.awssqsclient.pool;
 
-import com.amazonaws.services.sqs.buffered.AmazonSQSBufferedAsyncClient;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-
 public class AwsSQSClientPoolImplTest {
-
-    @Test
-    @DisplayName("Pool에있는 entry객체수를 조회")
-    void returnFlxiblePoolSize() {
-        AmazonSQSBufferedAsyncClient asyncClient = mock(AmazonSQSBufferedAsyncClient.class);
-
-        AwsSQSClientPoolImpl flexibleAwsSQSClientPool = new AwsSQSClientPoolImpl(new PoolConfiguration(), asyncClient);
-
-        assertThat(flexibleAwsSQSClientPool.getPoolSize()).isEqualTo(2);
-    }
 
 }

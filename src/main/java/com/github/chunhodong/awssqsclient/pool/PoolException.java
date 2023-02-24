a9f2d79975fb50159638e9cb3be2fbf6505ea136
chunhodong <yhms4432@naver.com>
2023-02-24 23:45:16 +0900

    refactor: PoolConfiguration에서 설정값검증추가

--- src/main/java/com/github/chunhodong/awssqsclient/pool/PoolException.java
+++ src/main/java/com/github/chunhodong/awssqsclient/pool/PoolException.java
@@ -2,7 +2,9 @@ package com.github.chunhodong.awssqsclient.pool;
 
 public class PoolException extends RuntimeException {
 
-    public static final String MIN_IDLE_TIMEOUT_EXCEPTION = "idletimeout value must be more than 10000";
+    public static final String MIN_IDLE_TIMEOUT_EXCEPTION = "idleTimeout value must be more than 10000";
+    public static final String MIN_CONNECTION_TIMEOUT_EXCEPTION = "connectionTimeout value must be more than 10000";
+    public static final String MIN_POOL_SIZE_EXCEPTION = "poolSize value must be more than 10";
     public static final String NEGATIVE_IDLE_TIMEOUT_EXCEPTION = "negative value are not allowed";
 
     public PoolException(String message) {
